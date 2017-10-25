930453212b5837332346017422087a1f801f8abe
Nate <npaulemon@gmail.com>
2017-10-25 17:09:02 -0400

    add classes of all nested elements to parent span

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
-	var html ='<p><span class="bold"><span class="italic">foobar</span></span></p>';
-	console.log(html);
-
+function removeNestedTags(html){
 	var dom = $(html);
 
 	console.log(dom.html());
+
+	dom.each(function(){
+		$(this)[0].className 
+		$(this).contents().each(function(index){
+			$(this)[0].className = $(this)[0].className + ' ' + $(this).contents()[index].className;
+		});
+	});
+
+	return dom.wrap('<p/>').parent().html();
+}
+
+$(document).ready(function(){
+	var html ='<p><span class="bold"><span class="italic">foobar</span></span></p>';	
+	console.log(removeNestedTags(html));
 });
