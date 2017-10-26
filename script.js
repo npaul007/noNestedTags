e8ec27573e4d0d28c041f816bb05ac1b42dce1f5
Nate <npaulemon@gmail.com>
2017-10-25 21:33:06 -0400

    roll back attributes to parent span as well

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ function removeNestedTags(html){
 
 	dom.each(function(index){
 		var classList = '';
+		var attr;
 	    $(this).find('span').each(function(){
 	        var self = $(this);
+
 	        if($(this).find('span').length != 1){
 				classList = classList + ' ' + self[0].className;
+
+				if(self[0].attributes.data)
+	        		attr = self[0].attributes.data.value;
+
 				self.contents().unwrap();
 	    	}
 	    });	
 
 	    $(this).children()[0].classList = $(this).children()[0].classList + classList
+	    $(this).children().attr('data',attr);
 	});
 
 	return dom;
 }
 
 $(document).ready(function(){
-	var html ='<p><span class="bold"><span class="italic"><span class="underline">foobar broski</span></span></span></p><p><span class="bro"><span class="blah"><span class="yass">wtf broski</span></span></span></p>';	
+	var html ='<p><span class="bold"><span class="italic"><span class="underline color" data="#fffff">foobar broski</span></span></span></p><p><span class="bro"><span class="blah"><span class="yass color" data="#fffff">wtf broski</span></span></span></p>';	
 	var nonNestedHTML = removeNestedTags(html);
+	console.log(nonNestedHTML[0]);
+	console.log(nonNestedHTML[1]);
 });
